632476dfe3edca7828a22f893ac3317ee831d946
Michael Misshore <michael@mudbugmedia.com>
2013-06-07 09:43:06 -0500

    Trimming whitespace

--- js/addremove.jquery.js
+++ js/addremove.jquery.js
@@ -2,17 +2,17 @@
 // Create dynamic forms with add/remove functionality
 //
 // Copyright (C) 2011 by Adam Clarke (github.com/vaxinate)
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,7 +23,7 @@
 
 (function($){
 
-	$.fn.extend({ 
+	$.fn.extend({
 
 		//pass the options variable to the function
 		addremove: function(options) {
@@ -33,26 +33,26 @@
 				plusbutton: '<a href="#" class="plus">+</a>',
 				minusbutton: '<a href="#" class="minus">-</a>',
 			}
-			
+
 			var options =  $.extend(defaults, options);
 			var $super = this;
 
 			return this.each(function() {
 				var opts = options;
-				
+
 				setup_buttons();
-								
+
 				function setup_buttons () {
 					// clear any buttons that are already done
 					var buttons = $super.find('.buttons');
 					$(buttons).children().remove();
-					
+
 					if (buttons.length > 1) {
 						// add minus button to all of them
 						buttons.each(function () {
 							$(this).html(opts.minusbutton);
 						});
-						
+
 						$super.find('.minus').click(function () {
 							$(this).parents('tr').remove();
 							renumber();
@@ -60,10 +60,10 @@
 							return false;
 						});
 					}
-					
+
 					// add plus button to last one
 					var $last_one = $($super.children('tbody').children(':last'));
-					
+
 					$last_one.children('.buttons').append(opts.plusbutton);
 					$last_one.find('.plus').click(function () {
 						var new_one = $last_one.clone();
@@ -75,10 +75,10 @@
 						return false''
 					});
 				}
-				
+
 				function renumber () {
 					var num = 0;
-					
+
 					$super.find('tr').each(function() {
 						$(this).find('input, textarea').each(function () {
 							$(this).prop('name', $(this).prop('name').replace(/\[\d*\]/, '['+num+']'));
